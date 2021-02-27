c8301699ff625e363173b357421739df9d1ac20d
SubvertDev <subvert.dev@gmail.com>
2021-02-27 20:18:57 +0300

    Fix no/bad location reading for long time

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -81,6 +81,11 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             return
         }
         
+        var distance = CLLocationDistance(Double.greatestFiniteMagnitude)
+        if let location = location {
+            distance = newLocation.distance(from: location)
+        }
+        
         if location == nil || location!.horizontalAccuracy > newLocation.horizontalAccuracy {
             lastLocationError = nil
             location = newLocation
@@ -88,6 +93,9 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
             if newLocation.horizontalAccuracy <= locationManager.desiredAccuracy {
                 print("We're done!")
                 stopLocationManager()
+                if distance > 0 {
+                    performingReversGeocoding = false
+                }
             }
             updateLabels()
             
@@ -106,6 +114,13 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
                     self.performingReversGeocoding = false
                     self.updateLabels()
                 }
+            }
+        } else if distance < 1 {
+            let timeInterval = newLocation.timestamp.timeIntervalSince(location!.timestamp)
+            if timeInterval > 10 {
+                print("Force done!")
+                stopLocationManager()
+                updateLabels()
             }
         }
     }
