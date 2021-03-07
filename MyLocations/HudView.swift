ea0d68ccc9b78aec2dae17bd2eef0abf380b88be
SubvertDev <subvert.dev@gmail.com>
2021-03-07 20:40:00 +0300

    Change animation to spring stuff

--- MyLocations/HudView.swift
+++ MyLocations/HudView.swift
@@ -53,10 +53,15 @@ class HudView: UIView {
         if animated {
             alpha = 0
             transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
-            UIView.animate(withDuration: 0.3) {
-                self.alpha = 1
-                self.transform = CGAffineTransform.identity
-            }
+            UIView.animate(withDuration: 0.3,
+                           delay: 0,
+                           usingSpringWithDamping: 0.7,
+                           initialSpringVelocity: 0.5,
+                           options: [],
+                           animations: {
+                                self.alpha = 1
+                                self.transform = CGAffineTransform.identity
+                           }, completion: nil)
         }
     }
 }
