b8c309861fa460fbdd4fabb9cf814cf6f6997c63
SubvertDev <subvert.dev@gmail.com>
2021-03-07 20:33:59 +0300

    Add animation to tag location (not complete)

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -53,7 +53,10 @@ class LocationDetailsViewController: UITableViewController {
     // MARK: - Actions
     
     @IBAction func done(_ sender: UIBarButtonItem) {
-        navigationController?.popViewController(animated: true)
+        guard let mainView = navigationController?.parent?.view else { return }
+        let hudView = HudView.hud(inView: mainView, animated: true)
+        hudView.text = "Tagged"
+        //navigationController?.popViewController(animated: true)
     }
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {
