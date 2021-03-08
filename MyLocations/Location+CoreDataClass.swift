1506e64ec1fbb40600988f1b71a0f7c84f79ecb6
SubvertDev <subvert.dev@gmail.com>
2021-03-08 21:31:56 +0300

    CoreData setup

---
+++ MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Subvert on 08.03.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
