ba3399a08cbd09b332817cf8c58aee518a91d543
SubvertDev <subvert.dev@gmail.com>
2021-03-13 23:33:34 +0300

    Add Location and User elements

---
+++ MyLocations/MapViewController.swift
@@ -0,0 +1,93 @@
+//
+//  MapViewController.swift
+//  MyLocations
+//
+//  Created by Subvert on 13.03.2021.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    var managedObjectContext: NSManagedObjectContext!
+    
+    var locations = [Location]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if !locations.isEmpty {
+            updateLocations()
+        }
+    }
+    //MARK: - Actions
+    @IBAction func showUser() {
+        let region = MKCoordinateRegion(center: mapView.userLocation.coordinate,
+                                        latitudinalMeters: 1000,
+                                        longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    @IBAction func showLocations() {
+        let theRegion = region(for: locations)
+        mapView.setRegion(theRegion, animated: true)
+    }
+    
+    //MARK: - Helper Methods
+    func updateLocations() {
+        mapView.removeAnnotations(locations)
+        
+        let entity = Location.entity()
+        
+        let fetchRequest = NSFetchRequest<Location>()
+        fetchRequest.entity = entity
+        
+        locations = try! managedObjectContext.fetch(fetchRequest)
+        mapView.addAnnotations(locations)
+    }
+    
+    func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
+        let region: MKCoordinateRegion
+        
+        switch annotations.count {
+        case 0:
+            region = MKCoordinateRegion(center: mapView.userLocation.coordinate,
+                                        latitudinalMeters: 1000,
+                                        longitudinalMeters: 1000)
+        case 1:
+            let annotation = annotations[annotations.count - 1]
+            region = MKCoordinateRegion(center: annotation.coordinate,
+                                        latitudinalMeters: 1000,
+                                        longitudinalMeters: 1000)
+        default:
+            var topLeft = CLLocationCoordinate2D(latitude: -90, longitude: 180)
+            var bottomRight = CLLocationCoordinate2D(latitude: 90, longitude: -180)
+            
+            for annotation in annotations {
+                topLeft.latitude = max(topLeft.latitude, annotation.coordinate.latitude)
+                topLeft.longitude = min(topLeft.longitude, annotation.coordinate.longitude)
+                bottomRight.latitude = min(bottomRight.latitude, annotation.coordinate.latitude)
+                bottomRight.longitude = max(bottomRight.longitude, annotation.coordinate.longitude)
+            }
+            let center = CLLocationCoordinate2D(
+                latitude: topLeft.latitude - (topLeft.latitude - bottomRight.latitude) / 2,
+                longitude: topLeft.longitude - (topLeft.longitude - bottomRight.longitude) / 2)
+            let extraSpace = 1.1
+            let span = MKCoordinateSpan(
+                latitudeDelta: abs(topLeft.latitude - bottomRight.latitude) * extraSpace,
+                longitudeDelta: abs(topLeft.longitude - bottomRight.longitude) * extraSpace)
+            
+            region = MKCoordinateRegion(center: center, span: span)
+        }
+        
+        return mapView.regionThatFits(region)
+    }
+    
+}
+
+extension MapViewController: MKMapViewDelegate {
+    
+}
--- MyLocations/SceneDelegate.swift
+++ MyLocations/SceneDelegate.swift
@@ -25,6 +25,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             navController = tabViewControllers[1] as! UINavigationController
             let controller2 = navController.viewControllers.first as! LocationsViewController
             controller2.managedObjectContext = managedObjectContext
+            
+            navController = tabViewControllers[2] as! UINavigationController
+            let controller3 = navController.viewControllers.first as! MapViewController
+            controller3.managedObjectContext = managedObjectContext
         }
         listenForFatalCoreDataNotifications()
         //guard let _ = (scene as? UIWindowScene) else { return }
